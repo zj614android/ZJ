0a6008a0de30cafda7f7eb2a0ac10682251fab10
2807738720@qq.com <2807738720@qq.com>
2018-10-08 10:24:32 +0800

    pull

--- app/src/main/java/android/zj/com/zjmine/base/BaseActivity.java
+++ app/src/main/java/android/zj/com/zjmine/base/BaseActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
